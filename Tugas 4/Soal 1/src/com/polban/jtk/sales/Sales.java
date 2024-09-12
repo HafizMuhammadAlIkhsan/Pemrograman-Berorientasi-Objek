e75d41568499351549e073fb307b4a770173634e
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-09-13 01:57:25 +0700

    :art: Menambahkan Tugas 4

---
+++ Tugas 4/Soal 1/src/com/polban/jtk/sales/Sales.java
@@ -0,0 +1,36 @@
+package com.polban.jtk.sales;
+
+import java.text.DecimalFormat;
+
+// Kelas Sales untuk mengelola transaksi penjualan
+public class Sales {
+    private Product product;
+
+    // Constructor
+    public Sales(Product product) {
+        this.product = product;
+    }
+
+    // Metode untuk melakukan penjualan
+    public void sellProduct(int quantity) {
+        System.out.println("Memproses penjualan...");
+        product.sellProduct(quantity);
+        System.out.println("Stok setelah penjualan: " + product.getStock());
+    }
+
+    // Metode untuk menambah stok produk
+    public void restockProduct(int quantity) {
+        System.out.println("Menambah stok...");
+        product.addStock(quantity);
+        System.out.println("Stok setelah penambahan: " + product.getStock());
+    }
+
+    // Metode untuk memperbarui harga produk
+    public void updateProductPrice(double newPrice) {
+        System.out.println("Memperbarui harga produk...");
+        product.setPrice(newPrice);
+        DecimalFormat desimal = new DecimalFormat("#,###.00");
+        System.out.println("Harga baru: " + desimal.format(product.getPrice()));
+    }
+}
+
---
+++ Tugas 4/Soal 2/src/Main.java
@@ -0,0 +1,9 @@
+import com.polban.jtk.inventory.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Inventori beli = new Inventori();
+        beli.pengadaan();
+    }
+}
+
