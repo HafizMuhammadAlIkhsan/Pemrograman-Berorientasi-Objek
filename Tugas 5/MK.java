2bc178245343570bc41f06d729ce17cc23fd3920
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-09-20 10:42:15 +0700

    :art:Menambahkan tugas ke 5

---
+++ Tugas 5/MK.java
@@ -0,0 +1,25 @@
+public class MK {
+    private String kodeMK;
+    private String namaMK;
+
+    public MK(String kodeMK, String namaMK) {
+        this.kodeMK = kodeMK;
+        this.namaMK = namaMK;
+    }
+
+    public String getKodeMK() {
+        return kodeMK;
+    }
+
+    public String getNamaMK() {
+        return namaMK;
+    }
+
+    public void setKodeMK(String kodeMK) {
+        this.kodeMK = kodeMK;
+    }
+
+    public void setNamaMK(String namaMK) {
+        this.namaMK = namaMK;
+    }
+}
