a425342110921d2211700d17f960a2d900fe2608
HafizMuhammadAlIkhsan <hafiz.muhammad.tif23@polban.ac.id>
2024-10-19 17:09:01 +0700

    Rename Folder

--- Tugas 4/Soal 2/src/Main.java
+++
@@ -1,9 +0,0 @@
-import com.polban.jtk.inventory.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Inventori beli = new Inventori();
-        beli.pengadaan();
-    }
-}
-
